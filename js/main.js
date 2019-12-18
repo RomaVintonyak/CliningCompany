c3cdf492c6f8235afd3561a09de4ec733fee8783
RomaVintonyak <19roman23@gmail.com>
2019-12-18 16:34:23 +0200

    create scroll top script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-$(function(){
-
+$(function() {
+  /*global variables*/
+  /*scrool top*/
+  var top_button = $("#back__top");
+  /*scrool to top function*/
+  top_button.hide();
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      top_button.fadeIn();
+    } else {
+      top_button.fadeOut();
+    }
+  });
+  top_button.click(function(event) {
+    event.preventDefault();
+    $("body, html").animate(
+      {
+        scrollTop: 0
+      },
+      800
+    );
+    return false;
+  });
 });
