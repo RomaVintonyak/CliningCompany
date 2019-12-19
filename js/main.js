9d8824b085e441ae409b3f21935034300553105b
RomaVintonyak <19roman23@gmail.com>
2019-12-19 14:56:28 +0200

    create media for project section

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ $(function() {
   /*scrool top*/
   var top_button = $("#back__top");
   /*our servicess */
- var btn_prev = $("#servicesBPrev");
- var btn_next = $("#servicesBNext"); 
+  var btn_prev = $("#servicesBPrev");
+  var btn_next = $("#servicesBNext");
+  /*filter category*/
+  var filter = $("[data-filter]");
   /*scrool to top function*/
   top_button.hide();
   $(window).scroll(function() {
@@ -25,12 +27,30 @@ $(function() {
     return false;
   });
   /*our servicess */
-  btn_prev.on("click", function(event){
+  btn_prev.on("click", function(event) {
     event.preventDefault();
     $("#services__card1").toggleClass("active__card");
   });
-  btn_next.on("click", function(event){
+  btn_next.on("click", function(event) {
     event.preventDefault();
     $("#services__card3").toggleClass("active__card");
+  });
+  /*filter category*/
+  filter.on("click", function(event) {
+    event.preventDefault();
+    $(this).toggleClass("link__active");
+    var cat = $(this).data("filter");
+    if (cat == "all") {
+      $("[data-cat]").removeClass("hide");
+    } else {
+      $("[data-cat]").each(function() {
+        var workCat = $(this).data("cat");
+        if (workCat != cat) {
+          $(this).addClass("hide");
+        } else {
+          $(this).removeClass("hide");
+        }
+      });
+    }
   });
 });
