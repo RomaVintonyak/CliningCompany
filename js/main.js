bd943f0cb350a0c091a721532fb6d1d5466ff46e
RomaVintonyak <19roman23@gmail.com>
2019-12-19 12:29:33 +0200

    createmedia for our services

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(function() {
   /*global variables*/
   /*scrool top*/
   var top_button = $("#back__top");
+  /*our servicess */
+ var btn_prev = $("#servicesBPrev");
+ var btn_next = $("#servicesBNext"); 
   /*scrool to top function*/
   top_button.hide();
   $(window).scroll(function() {
@@ -20,5 +23,14 @@ $(function() {
       800
     );
     return false;
+  });
+  /*our servicess */
+  btn_prev.on("click", function(event){
+    event.preventDefault();
+    $("#services__card1").toggleClass("active__card");
+  });
+  btn_next.on("click", function(event){
+    event.preventDefault();
+    $("#services__card3").toggleClass("active__card");
   });
 });
